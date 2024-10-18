b7b9388461ac54be930725e04a8fb191a709d610
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-10-18 12:54:12 +0200

    errors, photo, validators(water,user)

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,10 @@ import {
 } from '../controllers/user.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import isValidId from '../middlewares/isValidId.js';
+import { upload } from '../middlewares/upload.js';
+import validationBody from '../middlewares/validationBody.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { createUserSchema, updateUserSchema } from '../validations/users.js';
 
 const router = Router();
 
@@ -18,8 +21,19 @@ router.get('/', ctrlWrapper(getAllUsersController));
 
 router.get('/:id', isValidId, ctrlWrapper(getUserController));
 
-router.post('/', ctrlWrapper(createUserController));
+router.post(
+  '/',
+  upload.single('photo'),
+  validationBody(createUserSchema),
+  ctrlWrapper(createUserController),
+);
 
-router.patch('/:id', ctrlWrapper(updateUserController));
+router.patch(
+  '/:id',
+  isValidId,
+  upload.single('photo'),
+  validationBody(updateUserSchema),
+  ctrlWrapper(updateUserController),
+);
 
 export default router;
---
+++ src/validations/users.js
@@ -0,0 +1,47 @@
+import Joi from 'joi';
+
+export const createUserSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Name should be a string.',
+    'string.min': 'Name should be at least {#limit} characters.',
+    'string.max': 'Name should be at most {#limit} characters.',
+    'any.required': 'Name is required.',
+  }),
+  email: Joi.string().email().optional().required().messages({
+    'string.email': 'Email must be a valid email address!',
+    'any.required': 'Email is required.',
+  }),
+  password: Joi.string().required().messages({
+    'string.base': 'Password must be a string.',
+    'any.required': 'Password is required.',
+  }),
+  gender: Joi.string().valid('male', 'female').messages({
+    'string.base': 'Gender must be a string.',
+    'any.only': 'Gender must be one of {#valids}.',
+  }),
+});
+
+export const updateUserSchema = Joi.object({
+  email: Joi.string().email().messages({
+    'string.email': 'Email must be a valid email address!',
+  }),
+  password: Joi.string().messages({
+    'string.base': 'Password must be a string.',
+  }),
+  userName: Joi.string().min(3).max(30).messages({
+    'string.base': 'Username should be a string.',
+    'string.min': 'Username should be at least {#limit} characters.',
+    'string.max': 'Username should be at most {#limit} characters.',
+  }),
+  gender: Joi.string().valid('male', 'female').messages({
+    'string.base': 'Gender must be a string.',
+    'any.only': 'Gender must be one of {#valids}.',
+  }),
+  waterRate: Joi.number().messages({
+    'number.base': 'Water rate must be a number.',
+  }),
+  photo: Joi.string().uri().messages({
+    'string.base': 'Photo must be a string.',
+    'string.uri': 'Photo must be a valid URI.',
+  }),
+}).min(1);
