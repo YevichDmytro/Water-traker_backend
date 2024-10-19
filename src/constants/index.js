50c52a297849aa283ea189e317d2962f9d358af8
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-10-19 18:25:15 +0200

    correct last

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,7 +7,7 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
+  CLOUD_NAME: 'CLOUDINARY_CLOUD_NAME',
+  API_KEY: 'CLOUDINARY_API_KEY',
+  API_SECRET: 'CLOUDINARY_API_SECRET',
 };
