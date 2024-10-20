e16d4b67c5ec7352fbd48dd13cbab546af51d682
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-20 19:04:58 +0300

    delete refresh

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,5 @@
 import { ONE_DAY } from '../constants/index.js';
-import UsersCollection from '../db/models/Users.js';
-import {
-  registerUser,
-  loginUser,
-  logoutUser,
-  refreshUsersSession,
-} from '../services/auth.js';
+import { registerUser, loginUser, logoutUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -48,32 +42,4 @@ export const logoutUserController = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
-};
-
-const setupSession = (res, session) => {
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
-};
-
-export const refreshUserSessionController = async (req, res) => {
-  const session = await refreshUsersSession({
-    sessionId: req.cookies.sessionId,
-    refreshToken: req.cookies.refreshToken,
-  });
-
-  setupSession(res, session);
-
-  res.json({
-    status: 200,
-    message: 'Successfully refreshed a session!',
-    data: {
-      accessToken: session.accessToken,
-    },
-  });
 };
