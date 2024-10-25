a52599142798643e9273320335f560a09fd37a84
Oleksandr <olexandr.veseluy@gmail.com>
2024-10-25 18:22:34 +0300

    update water by month

---
+++ src/utils/formatDateTime.js
@@ -0,0 +1,7 @@
+const formatDateTime = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  return `${year}-${month}`;
+};
+
+export default formatDateTime;
