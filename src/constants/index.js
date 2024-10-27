020488888578a221ca0613a391fcd172f2305d18
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-10-27 12:06:08 +0100

    123

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
 import path from 'node:path';
-// import * as path from 'node:path';
 
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const FIFTEEN_MINUTES = 1 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
