0b7e96c6e0d44cb453cd716cf0b5e5ab7a5a3ba4
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-10-30 17:12:01 +0200

    Revert "Google auth for checking"

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,12 +4,10 @@ import {
   registerUserController,
   loginUserController,
   logoutUserController,
-  getOAuthUrl,
-  loginWithGoogle,
 } from '../controllers/auth.js';
 import validationBody from '../middlewares/validationBody.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
-import { authUserSchema, loginWithGoogleSchema } from '../validations/auth.js';
+import { authUserSchema } from '../validations/auth.js';
 
 const router = Router();
 
@@ -26,13 +24,5 @@ router.post(
 );
 
 router.post('/logout', ctrlWrapper(logoutUserController));
-
-router.get('/get-oauth-url', ctrlWrapper(getOAuthUrl));
-
-router.post(
-  '/confirm-oauth',
-  validationBody(loginWithGoogleSchema),
-  ctrlWrapper(loginWithGoogle),
-);
 
 export default router;
